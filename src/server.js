8cc5b6127ea4a66595e07777eb7e0d780d8c1faa
Rama Ayodhya <ramaaydhya@gmail.com>
2022-12-17 13:13:02 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const Hapi = require('@hapi/hapi');
+const notes = require('./api/notes');
+const NotesServices = require('./services/inMemory/NotesService')
+const NotesValidator = require('./validator/notes')
+
+const init = async () => {
+  const notesService = new NotesServices();
+  const server = Hapi.server({
+    port: 5000,
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+      validator: NotesValidator,
+    }
+  })
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+
+init();
